50dbc8d0d6e07a143c21810511f95c18f70f29c5
Alexandre Salvador <62520840+alexandresalvador@users.noreply.github.com>
2022-11-23 22:00:14 -0300

    third commit

--- teste_wine.py
+++ teste_wine.py
@@ -25,7 +25,28 @@ for x in range(0, 2):
     acuracia = accuracy_score
     (predicao, label_test) * 100
     acuracias.append(acuracia)
+    print("Acurácia: ", acuracia)
 
+print('med: ', np.mean( acuracia ))
+
+print('min: ', np.min( acuracias ))
+
+print('max: ', np.max( acuracias ))
+
+dados01 = [[13.71,1.86,2.36,16.6,101,2.61,2.88,.27,1.69,3.8,1.11,4,1035]] #1
+
+dados02 = [[12,1.51,2.42,22,86,1.45,1.25,.5,1.63,3.6,1.05,2.65,450]] #2
+
+dados03 = [[13.17,5.19,2.32,22,93,1.74,.63,.61,1.55,7.9,.6,1.48,725]] #3
+
+predicao01 = model.predict(dados01)
+predicao02 = model.predict(dados02)
+predicao03 = model.predict(dados03)
+
+
+print('Prediction 01: ', predicao01)
+print('Prediction 02: ', predicao02)
+print('Prediction 03: ', predicao03)
 
 print("Matriz de confusão")
 print(confusion_matrix(predicao, label_test))
